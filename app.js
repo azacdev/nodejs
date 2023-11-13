b989d70bb4deaf3ed065b68855c856bed8100b7a
azacdev <azacdev@gmail.com>
2023-11-13 23:54:05 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const lodash = require("lodash");
+const _ = require("lodash");
 
-const items = [1[2, [3, [4]]]];
+const items = [1,[2, [3, [4]]]];
 
-const newItems =  flattenDeep(items);
+const newItems =  _.flattenDeep(items)
 
 console.log(newItems);
