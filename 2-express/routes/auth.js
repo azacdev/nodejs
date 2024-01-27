5e1a9ce99e3105addb6f0c6566ea67eea1111206
azacdev <azacdev@gmail.com>
2024-01-27 23:50:29 +0100

    update

--- 2-express/routes/auth.js
+++ 2-express/routes/auth.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.post("/", (req, res) => {
   const { name } = req.body;
   if (!name) {
-    res.status(201).send("please provide the required credentials");
+    res.status(201).send("Please provide the required credentials");
   }
   res.status(200).send("Welcome" + " " + name);
 });
